c9c66a14282c87398db190fcdde93571d8a1e029
Sam Oberly <samob917@outlook.com>
2025-03-04 22:37:53 -0500

    Create card for books
    Cards are created for each book in the list. I do not like how
    this is handled. I need to look up how to create more complex
    html in js.

--- library.js
+++ library.js
@@ -1,1 +1,40 @@
-console.log("Hello, console")
+const myLibrary = [];
+
+function Book(title, pages, author) {
+    this.title = title;
+    this.pages = pages;
+    this.author = author;
+    this.details = function() {
+        return `${this.title}, by ${this.author}, is ${this.pages} long.`
+    };
+}
+
+function addBookToLibrary(title, pages, author) {
+    const newBook = new Book(title, pages, author);
+    myLibrary.push(newBook);
+}
+
+console.log(myLibrary);
+addBookToLibrary("You Like It Darker", 450, "Stephen King");
+console.log(myLibrary);
+
+function displayLibrary() {
+    const library = document.querySelector(".lib");
+    for (book of myLibrary) {
+        const bookDiv = document.createElement("div");
+        const bookTitle = document.createElement("h2");
+        bookTitle.textContent = book.title;
+        const bookPages = document.createElement("p")
+        bookPages.textContent = `Pages: ${book.pages}`;
+        const bookAuthor = document.createElement("h3");
+        bookAuthor.textContent = `By: ${book.author}`
+        bookDiv.appendChild(bookTitle);
+        bookDiv.appendChild(bookAuthor);
+        bookDiv.appendChild(bookPages);
+        library.appendChild(bookDiv);
+    }
+}
+
+displayLibrary();
+
+
