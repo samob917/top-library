eac0eb66efbdfa13439bed0842750ed0768e9c03
Sam Oberly <samob917@outlook.com>
2025-03-05 20:37:34 -0500

    Add modal and operating buttons
    Added buttons to open and close a modal. Now I need a form inside this
    that lets someone add a book.

--- library.js
+++ library.js
@@ -51,3 +51,14 @@ function displayLibrary() {
 displayLibrary();
 
 
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+showButton.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+    dialog.close();
+});
