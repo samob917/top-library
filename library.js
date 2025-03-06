1ce0b830e113dc4fda2d74829d05c79219422b96
Sam Oberly <samob917@outlook.com>
2025-03-06 12:21:34 -0500

    Handle remove book button
    Button to remove book works and changes the list of my books.
    Note that I made the list mutable and use a unique UUID to get
    the book and div to be deleted.

--- library.js
+++ library.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+let myLibrary = [];
 
 function Book(title, pages, author, read) {
     this.title = title;
@@ -22,7 +22,14 @@ addBookToLibrary("Number Go Up", 260, "Zeke Faux", true);
 addBookToLibrary("Number Go Up", 260, "Zeke Faux", false);
 addBookToLibrary("Number Go Up", 260, "Zeke Faux", false);
 addBookToLibrary("Number Go Up", 260, "Zeke Faux", false);
-console.log(myLibrary);
+
+function handleDelete(e) {
+    const parentDiv = e.target.parentNode;
+    const library = parentDiv.parentNode;
+    library.removeChild(parentDiv);
+    const id = parentDiv.dataset.indexNumber;
+    myLibrary = myLibrary.filter(book => book.id != id)
+}
 
 function displayLibrary() {
     const library = document.querySelector(".lib");
@@ -30,8 +37,10 @@ function displayLibrary() {
         library.removeChild(library.firstChild);
     }
     for (book of myLibrary) {
+        let uuid = crypto.randomUUID();
+        book.id = uuid;
         const bookDiv = document.createElement("div");
-        // bookDiv.setAttribute("id", )
+        bookDiv.setAttribute("data-index-number", uuid);
         bookDiv.classList.add("book");
         if (book.read) {
             bookDiv.setAttribute("style", "background: green;");
@@ -44,14 +53,22 @@ function displayLibrary() {
         bookAuthor.textContent = `By: ${book.author}`
         const bookPages = document.createElement("p")
         bookPages.textContent = `Pages: ${book.pages}`;
+
+        const removeBook = document.createElement("button");
+        removeBook.setAttribute("style", "gray: red; color: red;")
+        removeBook.textContent = "Remove";
+        removeBook.addEventListener("click", handleDelete);
+
         bookDiv.appendChild(bookTitle);
         bookDiv.appendChild(bookAuthor);
         bookDiv.appendChild(bookPages);
+        bookDiv.appendChild(removeBook);
         library.appendChild(bookDiv);
     }
 }
 
 displayLibrary();
+console.log(myLibrary);
 
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button");
