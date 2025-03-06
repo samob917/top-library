dfda987e5e82df75e2deae85283ad05556189a7a
Sam Oberly <samob917@outlook.com>
2025-03-05 20:26:47 -0500

    Add books to display and style them
    Books are added to the display and styled with a background
    color showing if they have been read or not.

--- library.js
+++ library.js
@@ -1,33 +1,46 @@
 const myLibrary = [];
 
-function Book(title, pages, author) {
+function Book(title, pages, author, read) {
     this.title = title;
     this.pages = pages;
     this.author = author;
+    this.read = read;
     this.details = function() {
         return `${this.title}, by ${this.author}, is ${this.pages} long.`
     };
 }
 
-function addBookToLibrary(title, pages, author) {
-    const newBook = new Book(title, pages, author);
+function addBookToLibrary(title, pages, author, read) {
+    const newBook = new Book(title, pages, author, read);
     myLibrary.push(newBook);
 }
 
 console.log(myLibrary);
-addBookToLibrary("You Like It Darker", 450, "Stephen King");
+addBookToLibrary("You Like It Darker", 450, "Stephen King", true);
+addBookToLibrary("Number Go Up", 260, "Zeke Faux", false);
+addBookToLibrary("Number Go Up", 260, "Zeke Faux", true);
+addBookToLibrary("Number Go Up", 260, "Zeke Faux", false);
+addBookToLibrary("Number Go Up", 260, "Zeke Faux", false);
+addBookToLibrary("Number Go Up", 260, "Zeke Faux", false);
 console.log(myLibrary);
 
 function displayLibrary() {
     const library = document.querySelector(".lib");
     for (book of myLibrary) {
         const bookDiv = document.createElement("div");
+        // bookDiv.setAttribute("id", )
+        bookDiv.classList.add("book");
+        if (book.read) {
+            bookDiv.setAttribute("style", "background: green;");
+        } else {
+            bookDiv.setAttribute("style", "background: red;");
+        }
         const bookTitle = document.createElement("h2");
         bookTitle.textContent = book.title;
-        const bookPages = document.createElement("p")
-        bookPages.textContent = `Pages: ${book.pages}`;
         const bookAuthor = document.createElement("h3");
         bookAuthor.textContent = `By: ${book.author}`
+        const bookPages = document.createElement("p")
+        bookPages.textContent = `Pages: ${book.pages}`;
         bookDiv.appendChild(bookTitle);
         bookDiv.appendChild(bookAuthor);
         bookDiv.appendChild(bookPages);
